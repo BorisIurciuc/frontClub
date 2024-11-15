6dd73ca7fbf59faea83a4d2b7a3aaaea19969d34
Denis Nejelschi <nejelschi@gmail.com>
2024-11-15 15:42:22 +0100

    reg confirm

--- src/components/registrationConfirm/RegistrationConfirmed.tsx
+++ src/components/registrationConfirm/RegistrationConfirmed.tsx
@@ -1,14 +1,17 @@
-import "./registrationConfirmed.css"
-
+import { useSearchParams } from "react-router-dom";
 
 const RegistrationConfirmed = () => {
-    return (
-      <div className="registration-container">
-        <h2>Registration Confirmed!</h2>
-        <p>Your account has been successfully activated. You can now log in.</p>
-        <a href="/#/login">Go to Login</a>
-      </div>
-    );
-  };
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get("code");
+
+  console.log("Received confirmation code:", code);
+
+  return (
+    <div>
+      <h1>Registration Confirmed</h1>
+      <p>Your confirmation code: {code}</p>
+    </div>
+  );
+};
 
 export default RegistrationConfirmed;
