0bcfe8f86c36daa98583bf13188f532637e11d59
Denis Nejelschi <nejelschi@gmail.com>
2024-11-15 20:16:03 +0100

    back confirm user

--- src/components/registrationConfirm/RegistrationConfirmed.tsx
+++ src/components/registrationConfirm/RegistrationConfirmed.tsx
@@ -3,13 +3,20 @@ import { useSearchParams } from "react-router-dom";
 const RegistrationConfirmed = () => {
   const [searchParams] = useSearchParams();
   const code = searchParams.get("code");
+useEffect(()=>{
+  fetch("")
+},[])
+  console.log("Confirmation code:", code);
 
-  console.log("Received confirmation code:", code);
+  if (!code) {
+    return <p>Error: No confirmation code provided.</p>;
+  }
 
   return (
-    <div>
-      <h1>Registration Confirmed</h1>
-      <p>Your confirmation code: {code}</p>
+    <div className="registration-container">
+      <h2>Registration Confirmed!</h2>
+      <p>Your account has been successfully activated. You can now log in.</p>
+      <a href="/#/login">Go to Login</a>
     </div>
   );
 };
